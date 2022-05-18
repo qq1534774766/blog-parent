c86845501a57cb18423d7e9a9d3e939b011a94ab
aguo <aguowqg@qq.com>
2022-05-18 19:53:36 +0800

    1.添加redis配置类方法，实现集群配置，与application.yaml可共存

--- blog-api/src/main/java/com/aguo/blogapi/config/RedisConfig.java
+++ blog-api/src/main/java/com/aguo/blogapi/config/RedisConfig.java
@@ -1,7 +1,15 @@
 package com.aguo.blogapi.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -12,6 +20,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfig {
+    @Autowired
+    private RedisProperties redisProperties;
     @Bean
     public RedisTemplate<Object,Object> redisStringTemplate(RedisTemplate<Object, Object> redisTemplate){
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
@@ -19,4 +29,17 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(stringRedisSerializer);
         return redisTemplate;
     }
+    @Bean
+    public RedisClusterConfiguration redisClusterConfig(){
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        redisClusterConfiguration.clusterNode("120.76.137.145",6379)
+                .clusterNode("120.76.137.145",6380)
+                .clusterNode("120.76.137.145",6381);
+        redisClusterConfiguration.setMaxRedirects(3);
+        redisClusterConfiguration.setPassword("r08874151597");
+
+        return redisClusterConfiguration;
+    }
+
+
 }
